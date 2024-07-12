19db38526d23cb9098806a59556a6324d4914180
gauravgangwarweb <gauravgangwar501@gmail.com>
2024-07-12 22:18:46 +0530

    chore: Update button styles and text in PlayArea component
    This commit updates the button styles and text in the PlayArea component. The "CONFIRM ROLL" button now has a new class name "button" and a gradient background. The "MINT TO PLAY" button has been modified to use a span element for better styling and added a black background on hover. These changes improve the visual appeal and user experience of the buttons.

--- src/app/components/PlayArea.jsx
+++ src/app/components/PlayArea.jsx
@@ -25,13 +25,13 @@ const PlayArea = () => {
       <div className="mt-6 md:mt-0">
         {play ? (
           <div className="flex flex-col md:flex-row gap-10">
-            <button className="roll-button">ROLL (1/10)</button>
-            <button className="button"><span>CONFIRM ROLL</span></button>
+            <button className="roll-button px-[60px] md:px-[70px] py-[15px] md:py-[10px]">ROLL (1/10)</button>
+            <button className="confirm-button flex justify-center items-center"><span>CONFIRM ROLL</span></button>
           </div>
         ) : (
           <button
             onClick={() => setPlay(true)}
-            className="text-[#A6FFEE] text-lg font-semibold button"
+            className="text-[#A6FFEE] text-lg font-semibold button flex justify-center items-center"
           >
             <span>MINT TO PLAY</span>
           </button>
